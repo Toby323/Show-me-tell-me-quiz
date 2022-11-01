7ee5c51a32c95c103ad2ee21124fc13e4f784ee7
Toby Black <toby.black298@gmail.com>
2022-11-01 21:03:37 +0000

    code rafactor

--- src/App.jsx
+++ src/App.jsx
@@ -9,82 +9,78 @@ let randomNumber = 0;
 let currentQuestion = 0;
 
 function App() {
-	const [showMeDisplay, setShowMeDisplay] = useState("none");
-	const [currentText, setText] = useState(
-		"To start press either the arrows or the random button"
-	);
-	const [qa, setQa] = useState("Question");
-	const [generatedNumber, setNumber] = useState(0);
+  const [showMeDisplay, setShowMeDisplay] = useState("none");
+  const [currentText, setText] = useState(
+    "To start press either the arrows or the random button"
+  );
+  const [questionOrAnswer, setQuestionOrAnswer] = useState("Question");
 
-	function showRandomQuestion() {
-		setQa("Question");
-		randomNumber = Math.floor(Math.random() * 13) + 1;
-		setNumber(randomNumber + 1);
-		setText(JSON.stringify(questions[randomNumber].question));
-	}
+  function showRandomQuestion() {
+    setQuestionOrAnswer("Question");
+    randomNumber = Math.floor(Math.random() * 13) + 1;
+    setText(JSON.stringify(questions[randomNumber].question));
+  }
 
-	function showRandomAnswer() {
-		setQa("Answer");
-		setText(JSON.stringify(answers[randomNumber].answer));
-	}
+  function showRandomAnswer() {
+    setQuestionOrAnswer("Answer");
+    setText(JSON.stringify(answers[randomNumber].answer));
+  }
 
-	function stepForward() {
-		if (currentQuestion !== 13) {
-			setQa("Question");
-			currentQuestion = randomNumber += 1;
-			setNumber(currentQuestion + 1);
-			setText(JSON.stringify(questions[currentQuestion].question));
-		}
-	}
+  function stepForward() {
+    if (currentQuestion !== 13) {
+      setQuestionOrAnswer("Question");
+      currentQuestion = randomNumber += 1;
+      setText(JSON.stringify(questions[currentQuestion].question));
+    }
+  }
 
-	function stepBack() {
-		if (currentQuestion !== 0 || randomNumber > 0) {
-			setQa("Question");
-			currentQuestion = randomNumber -= 1;
-			setNumber(currentQuestion + 1);
-			setText(JSON.stringify(questions[currentQuestion].question));
-		}
-	}
+  function stepBack() {
+    if (currentQuestion !== 0 || randomNumber > 0) {
+      setQuestionOrAnswer("Question");
+      currentQuestion = randomNumber -= 1;
+      setText(JSON.stringify(questions[currentQuestion].question));
+    }
+  }
 
-	return (
-		<div className="App">
-			<div id="Titlebar">
-				<div id="Information">
-					<h1>
-						{qa}: <b>{randomNumber}</b>
-					</h1>
-				</div>
-			</div>
-			<div id="Question-Container">
-				<p>{currentText}</p>
-			</div>
-			<div id="Control-Bar">
-				<div id="Reveal-Answer-Button" onClick={showRandomAnswer}>
-					<h4 style={{ fontWeight: 700 }}>reveal</h4>
-				</div>
-				<BsArrowLeft
-					className="Icon-Button"
-					id="Step-Backward-Button"
-					color="white"
-					fontSize={70}
-					onClick={stepBack}
-				/>
-				<div
-					className="Icon-Button"
-					id="Show-Random-Question-Button"
-					onClick={showRandomQuestion}>
-					<GiPerspectiveDiceSixFacesRandom id="Random-Question-Icon" />
-				</div>
-				<BsArrowRight
-					className="Icon-Button"
-					id="Step-Forward-Button"
-					color="white"
-					fontSize={70}
-					onClick={stepForward}
-				/>
-			</div>
-		</div>
-	);
+  return (
+    <div className="App">
+      <div id="Titlebar">
+        <div id="Information">
+          <h1>
+            {questionOrAnswer}: <b>{randomNumber}</b>
+          </h1>
+        </div>
+      </div>
+      <div id="Question-Container">
+        <p>{currentText}</p>
+      </div>
+      <div id="Control-Bar">
+        <div id="Reveal-Answer-Button" onClick={showRandomAnswer}>
+          <h4 style={{ fontWeight: 700 }}>reveal</h4>
+        </div>
+        <BsArrowLeft
+          className="Icon-Button"
+          id="Step-Backward-Button"
+          color="white"
+          fontSize={70}
+          onClick={stepBack}
+        />
+        <div
+          className="Icon-Button"
+          id="Show-Random-Question-Button"
+          onClick={showRandomQuestion}>
+          <GiPerspectiveDiceSixFacesRandom id="Random-Question-Icon" />
+        </div>
+        <BsArrowRight
+          className="Icon-Button"
+          id="Step-Forward-Button"
+          color="white"
+          fontSize={70}
+          onClick={stepForward}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
